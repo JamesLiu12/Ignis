9f3f043d872f86b190938bccd50ba7dc0a7686a2
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2025-09-05 20:29:48 +0800

    chore: add an example for event system

---
+++ Ignis/src/Ignis/Examples/EventExample.cpp
@@ -0,0 +1,22 @@
+#include "EventExample.h"
+
+namespace ignis
+{
+	void EventExample()
+	{
+		EventDispatcher<KeyEvents> dispatcher;
+		auto subscription = dispatcher.Subscribe<KeyPressedEvent>(KeyEvents::KeyPressed,
+			[](KeyPressedEvent& event) {
+				printf("Key Pressed: %d (repeat = %s)\n",
+					event.GetKeyCode(),
+					event.IsRepeat() ? "true" : "false");
+				event.Handled = true;
+			});
+
+		while (true)
+		{
+			int ch = getchar();
+			dispatcher.Dispatch<KeyPressedEvent>(ch, false);
+		}
+	}
+}
