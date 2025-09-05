d62c2a5f2b017f6248092c56eb0ce507e4cab6d2
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2025-09-05 17:51:02 +0800

    fix: fixed typo and syntax errors

--- Ignis/src/Ignis/Core/Application.cpp
+++ Ignis/src/Ignis/Core/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Ignis/Events/Event.h"
+#include "Ignis/Events/KeyEvents.h"
+
 namespace ignis {
 
 	Application::Application()
@@ -12,7 +15,18 @@ namespace ignis {
 
 	void Application::Run()
 	{
+		EventDispatcher<KeyEvents> dispatcher;
+		auto subscription = dispatcher.Subscribe(KeyEvents::KeyPressed, [](Event<KeyEvents>& e) {
+			auto& event = static_cast<KeyPressedEvent&>(e);
+			printf("Key Pressed: %d (repeat = %s)\n", event.GetKeyCode(), event.IsRepeat() ? "true" : "false");
+			e.Handled = true;
+		});
 
-		while (true);
+		while (true)
+		{
+			int ch = getchar();
+			auto e = KeyPressedEvent(ch);
+			dispatcher.Dispatch(e);
+		}
 	}
 }
