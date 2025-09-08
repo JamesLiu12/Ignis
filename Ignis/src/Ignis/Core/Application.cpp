17bd6f18c37330b1259c6fbddd6462fa631ed26d
Chai Ming How <125442405+ChaiMingHow@users.noreply.github.com>
2025-09-08 11:36:56 +0800

    refactor: use inline functions and templates for log.h

--- Ignis/src/Ignis/Core/Application.cpp
+++ Ignis/src/Ignis/Core/Application.cpp
@@ -9,22 +9,22 @@ namespace ignis {
 		Log::Init();
 		
 		// Test logging system
-		IG_CORE_INFO("Ignis Engine initialized!");
-		IG_CORE_INFO_TAG("Core", "Application constructor called");
-		IG_INFO("Client application starting...");
-		IG_WARN_TAG("Test", "This is a warning message with tag");
-		IG_ERROR("This is an error message");
+		Log::core_info("Ignis Engine initialized!");
+		Log::core_info_tag("Core", "Application constructor called");
+		Log::info("Client application starting...");
+		Log::warn_tag("Test", "This is a warning message with tag");
+		Log::error("This is an error message");
 	}
 
 	Application::~Application()
 	{
-		IG_CORE_INFO("Application shutting down...");
+		Log::core_info("Application shutting down...");
 		Log::Shutdown();
 	}
 
 	void Application::Run()
 	{
-		IG_CORE_INFO_TAG("Core", "Application main loop started");
+		Log::core_info_tag("Core", "Application main loop started");
 		
 		// Simple application loop with sleep to prevent 100% CPU usage
 		while (m_Running)
@@ -33,6 +33,6 @@ namespace ignis {
 			std::this_thread::sleep_for(std::chrono::milliseconds(16)); // ~60 FPS
 		}
 		
-		IG_CORE_INFO_TAG("Core", "Application main loop ended");
+		Log::core_info_tag("Core", "Application main loop ended");
 	}
 }
