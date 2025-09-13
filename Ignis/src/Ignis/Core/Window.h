2ddbf6850c8c847ca8e67df3ace558b0dacc8a04
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2025-09-13 22:33:41 +0800

    fix: make apple-clang build

--- Ignis/src/Ignis/Core/Window.h
+++ Ignis/src/Ignis/Core/Window.h
@@ -12,10 +12,17 @@ namespace ignis
 	public:
 		struct WindowSpecs
 		{
-			std::string Title = "Ignis Engine";
-			uint32_t    Width = 1920;
-			uint32_t    Height = 1080;
-			bool        VSync = true;
+			std::string Title;
+			uint32_t    Width;
+			uint32_t    Height;
+			bool        VSync;
+
+			WindowSpecs(const std::string& title = "Ignis Engine",
+				uint32_t width = 1920,
+				uint32_t height = 1080,
+				bool vsync = true)
+				: Title(title), Width(width), Height(height), VSync(vsync) {
+			}
 		};;
 
 		using EventCallbackFn = std::function<void(EventBase&)>;
