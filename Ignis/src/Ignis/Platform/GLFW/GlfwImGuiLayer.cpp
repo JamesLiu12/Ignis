9998c53de7b3d997f6ec7d08319e0d63c703bcc7
Chai Ming How <125442405+ChaiMingHow@users.noreply.github.com>
2025-09-13 15:52:27 +0800

    refactor

--- Ignis/src/Ignis/Platform/GLFW/GlfwImGuiLayer.cpp
+++ Ignis/src/Ignis/Platform/GLFW/GlfwImGuiLayer.cpp
@@ -9,6 +9,7 @@
 
 // OpenGL headers
 #ifdef __APPLE__
+    #define GL_SILENCE_DEPRECATION
     #define GL_GLEXT_PROTOTYPES
     #define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
     #include <OpenGL/gl3.h>
