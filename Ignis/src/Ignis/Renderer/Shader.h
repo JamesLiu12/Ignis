ff432080cd1b69e79f1c93f1f51dfa4e0bc93b72
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2025-10-05 21:38:15 +0800

    feat: add vertex array

--- Ignis/src/Ignis/Renderer/Shader.h
+++ Ignis/src/Ignis/Renderer/Shader.h
@@ -16,6 +16,62 @@ namespace ignis
 			Mat3, Mat4
 		};
 
+		static constexpr size_t DataTypeSize(DataType type) noexcept
+		{
+			switch (type)
+			{
+			case DataType::Float:  return sizeof(float);
+			case DataType::Float2: return sizeof(float) * 2;
+			case DataType::Float3: return sizeof(float) * 3;
+			case DataType::Float4: return sizeof(float) * 4;
+
+			case DataType::Bool:   return sizeof(bool);
+			case DataType::Bool2:  return sizeof(bool) * 2;
+			case DataType::Bool3:  return sizeof(bool) * 3;
+			case DataType::Bool4:  return sizeof(bool) * 4;
+
+			case DataType::Int:    return sizeof(int32_t);
+			case DataType::Int2:   return sizeof(int32_t) * 2;
+			case DataType::Int3:   return sizeof(int32_t) * 3;
+			case DataType::Int4:   return sizeof(int32_t) * 4;
+
+			case DataType::UInt:   return sizeof(uint32_t);
+			case DataType::UInt2:  return sizeof(uint32_t) * 2;
+			case DataType::UInt3:  return sizeof(uint32_t) * 3;
+			case DataType::UInt4:  return sizeof(uint32_t) * 4;
+
+			case DataType::Mat3:   return sizeof(float) * 3 * 3;
+			case DataType::Mat4:   return sizeof(float) * 4 * 4;
+			default:               return 0;
+			}
+		}
+
+		static constexpr size_t DataTypeComponentCount(DataType type) noexcept
+		{
+			switch (type)
+			{
+			case DataType::Float:  return 1;
+			case DataType::Float2: return 2;
+			case DataType::Float3: return 3;
+			case DataType::Float4: return 4;
+			case DataType::Bool:   return 1;
+			case DataType::Bool2:  return 2;
+			case DataType::Bool3:  return 3;
+			case DataType::Bool4:  return 4;
+			case DataType::Int:    return 1;
+			case DataType::Int2:   return 2;
+			case DataType::Int3:   return 3;
+			case DataType::Int4:   return 4;
+			case DataType::UInt:   return 1;
+			case DataType::UInt2:  return 2;
+			case DataType::UInt3:  return 3;
+			case DataType::UInt4:  return 4;
+			case DataType::Mat3:   return 3;
+			case DataType::Mat4:   return 4;
+			default:               return 0;
+			}
+		}
+
 		virtual ~Shader() = default;
 
 		virtual void Bind() = 0;
