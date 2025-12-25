5ba7bc74c066f1ceb870e43d67a15760152606f9
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2025-12-25 20:17:43 +0800

    feat: add black and white textures

--- Ignis/src/Ignis/Renderer/Renderer.cpp
+++ Ignis/src/Ignis/Renderer/Renderer.cpp
@@ -1,6 +1,7 @@
 #include "Renderer.h"
 #include "GraphicsAPI.h"
 #include "Ignis/Platform/OpenGL/GLRenderer.h"
+#include "Texture.h"
 
 namespace ignis
 {
@@ -13,5 +14,35 @@ namespace ignis
 		default:
 			return nullptr;
 		}
+	}
+
+	const std::shared_ptr<Texture2D> Renderer::GetWhiteTexture()
+	{
+		static constexpr std::array<std::byte, 4> s_white_pixel =
+		{ std::byte{ 255 } ,std::byte{ 255 } ,std::byte{ 255 } ,std::byte{ 255 } };
+		static const std::span<const std::byte> s_white_data(s_white_pixel);
+
+		ignis::TextureSpecs specs;
+		specs.Width = 1;
+		specs.Height = 1;
+		specs.SourceFormat = ignis::ImageFormat::RGBA8;
+		specs.InternalFormat = ignis::ImageFormat::RGBA8;
+
+		return ignis::Texture2D::Create(specs, s_white_data);
+	}
+
+	const std::shared_ptr<Texture2D> Renderer::GetBlackTexture()
+	{
+		static constexpr std::array<std::byte, 4> s_black_pixel =
+		{ std::byte{ 0 } ,std::byte{ 0 } ,std::byte{ 0 } ,std::byte{ 0 } };
+		static const std::span<const std::byte> s_black_data(s_black_pixel);
+
+		ignis::TextureSpecs specs;
+		specs.Width = 1;
+		specs.Height = 1;
+		specs.SourceFormat = ignis::ImageFormat::RGBA8;
+		specs.InternalFormat = ignis::ImageFormat::RGBA8;
+
+		return ignis::Texture2D::Create(specs, s_black_data);
 	}
 }
