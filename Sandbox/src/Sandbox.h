f26ffd86bdfd295105a832b8a8efd87f6432f0a0
Chai Ming How <125442405+ChaiMingHow@users.noreply.github.com>
2025-12-26 19:48:13 +0800

    Fix: remove previous fix about vfs virtual path

--- Sandbox/src/Sandbox.h
+++ Sandbox/src/Sandbox.h
@@ -2,15 +2,6 @@
 
 #include "Ignis.h"
 
-// Platform-specific headers for executable path detection
-#if defined(__APPLE__)
-	#include <mach-o/dyld.h>  // For _NSGetExecutablePath
-#elif defined(_WIN32)
-	#include <windows.h>       // For GetModuleFileName
-#else
-	#include <unistd.h>        // For readlink
-#endif
-
 class Sandbox : public ignis::Application
 {
 public:
