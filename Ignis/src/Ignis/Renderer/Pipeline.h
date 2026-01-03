8225ac37a359e6f5e2b1015e8aac145e16fa1483
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2026-01-03 16:11:40 +0800

    feat: add rendering flow of Environment

--- Ignis/src/Ignis/Renderer/Pipeline.h
+++ Ignis/src/Ignis/Renderer/Pipeline.h
@@ -4,12 +4,16 @@
 
 namespace ignis
 {
+	class Environment;
+	struct LightEnvironment;
+
 	class Pipeline
 	{	
 	public:
 		virtual ~Pipeline() = default;
 
 		virtual std::shared_ptr<Material> CreateMaterial(const MaterialData& data) = 0;
+		virtual void ApplyEnvironment(Material& material, const Environment& scene_environment, const LightEnvironment& light_environment) = 0;
 		virtual std::shared_ptr<Shader> GetStandardShader() = 0;
 	};
 }
