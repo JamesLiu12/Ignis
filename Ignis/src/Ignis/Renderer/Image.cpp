cca9862c5adfe7be22a04f5e21676a2560931f33
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2026-01-03 18:06:13 +0800

    feat: add hdr image import

--- Ignis/src/Ignis/Renderer/Image.cpp
+++ Ignis/src/Ignis/Renderer/Image.cpp
@@ -9,48 +9,82 @@ namespace ignis
 			m_width = width;
 			m_height = height;
 			m_format = format;
-			m_pixels.resize(width * height * Channels(format));
-			std::memcpy(m_pixels.data(), data, width * height * Channels(format));
+
+			size_t size = width * height * BytesPerPixel(format);
+			m_pixels.resize(size);
+			std::memcpy(m_pixels.data(), data, size);
 			m_loaded = true;
 		}
 	}
 
 	std::shared_ptr<Image> Image::LoadFromFile(const std::filesystem::path& filepath, bool flip_vertical)
 	{
+		if (!std::filesystem::exists(filepath)) return nullptr;
+
 		stbi_set_flip_vertically_on_load(flip_vertical);
 
 		int width, height, channels;
+		void* data = nullptr;
+		ImageFormat format = ImageFormat::None;
+		size_t data_size = 0;
 
-		stbi_uc* data = stbi_load(filepath.string().c_str(), &width, &height, &channels, 0);
+		bool is_hdr = stbi_is_hdr(filepath.string().c_str());
+
+		if (is_hdr)
+		{
+			float* floatData = stbi_loadf(filepath.string().c_str(), &width, &height, &channels, 0);
+			data = floatData;
+
+			if (floatData)
+			{
+				if (channels == 4) format = ImageFormat::RGBA32F;
+				else if (channels == 3) format = ImageFormat::RGB32F;
+				else if (channels == 1) format = ImageFormat::R32F;
+				else {
+					// Fallback: Force load as 4 channels if it's an odd format
+					stbi_image_free(floatData);
+					floatData = stbi_loadf(filepath.string().c_str(), &width, &height, &channels, 4);
+					data = floatData;
+					format = ImageFormat::RGBA32F;
+					channels = 4;
+				}
+
+				data_size = width * height * channels * sizeof(float);
+			}
+		}
+		else
+		{
+			stbi_uc* byteData = stbi_load(filepath.string().c_str(), &width, &height, &channels, 0);
+			data = byteData;
+
+			if (byteData)
+			{
+				if (channels == 4) format = ImageFormat::RGBA8;
+				else if (channels == 3) format = ImageFormat::RGB8;
+				else if (channels == 1) format = ImageFormat::R8;
+				else {
+					// Fallback: Force load as 4 channels
+					stbi_image_free(byteData);
+					byteData = stbi_load(filepath.string().c_str(), &width, &height, &channels, 4);
+					data = byteData;
+					format = ImageFormat::RGBA8;
+					channels = 4;
+				}
+
+				data_size = width * height * channels * sizeof(stbi_uc);
+			}
+		}
 
 		if (!data) return nullptr;
 
 		auto image = std::make_shared<Image>();
 		image->m_width = width;
 		image->m_height = height;
+		image->m_format = format;
+		image->m_pixels.resize(data_size);
 
-		if (channels == 4)
-		{
-			image->m_format = ImageFormat::RGBA8;
-		}
-		else if (channels == 3)
-		{
-			image->m_format = ImageFormat::RGB8;
-		}
-		else if (channels == 1)
-		{
-			image->m_format = ImageFormat::R8;
-		}
-		else
-		{
-			stbi_image_free(data);
-			data = stbi_load(filepath.string().c_str(), &width, &height, &channels, 4);
-			image->m_format = ImageFormat::RGBA8;
-		}
-
-		size_t size = width * height * channels;
-		image->m_pixels.resize(size);
-		memcpy(image->m_pixels.data(), data, size);
+		std::memcpy(image->m_pixels.data(), data, data_size);
+		image->m_loaded = true;
 
 		stbi_image_free(data);
 		return image;
