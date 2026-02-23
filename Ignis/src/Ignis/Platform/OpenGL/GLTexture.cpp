f38f4faa9b288df282b8cde579dbd3dacb99e114
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2026-02-23 16:53:03 +0800

    feat: add "empty" constructor and SetData for Texture

--- Ignis/src/Ignis/Platform/OpenGL/GLTexture.cpp
+++ Ignis/src/Ignis/Platform/OpenGL/GLTexture.cpp
@@ -122,20 +122,6 @@ namespace ignis
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, ToGLMagFilter(specs.MagFilter));
 	}
 
-	void AllocateTextureStorage(const TextureSpecs& specs, ImageFormat source_format, const void* pixels)
-	{
-		const GLenum internal_format = ToGLInternalFormat(specs.Format);
-		const GLenum data_format = ToGLImageFormat(source_format);
-		const GLenum data_type = ToGLDataType(source_format);
-
-		glTexImage2D(GL_TEXTURE_2D, 0, internal_format, specs.Width, specs.Height, 0, data_format, data_type, pixels);
-
-		if (specs.GenMipmaps)
-		{
-			glGenerateMipmap(GL_TEXTURE_2D);
-		}
-	}
-
 	GLTexture2D::GLTexture2D(const TextureSpecs& specs, ImageFormat source_format, std::span<const std::byte> data)
 		: m_specs(specs)
 	{
@@ -151,10 +137,45 @@ namespace ignis
 		ApplyTextureParameters(m_specs);
 
 		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-		AllocateTextureStorage(m_specs, source_format, static_cast<const void*>(data.data()));
+		SetData(source_format, data);
 		glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
 
 		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+
+	GLTexture2D::GLTexture2D(const TextureSpecs& specs)
+		: m_specs(specs)
+	{
+		if (m_specs.Width == 0 || m_specs.Height == 0)
+		{
+			Log::CoreError("Texture dimensions must be non-zero for manual texture creation.");
+			return;
+		}
+
+		glGenTextures(1, &m_id);
+		glBindTexture(GL_TEXTURE_2D, m_id);
+
+		ApplyTextureParameters(m_specs);
+
+		const GLenum internal_format = ToGLInternalFormat(m_specs.Format);
+		glTexImage2D(GL_TEXTURE_2D, 0, internal_format, m_specs.Width, m_specs.Height, 0,
+			GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+
+	void GLTexture2D::SetData(ImageFormat source_format, std::span<const std::byte> data) const
+	{
+		const GLenum internal_format = ToGLInternalFormat(m_specs.Format);
+		const GLenum data_format = ToGLImageFormat(source_format);
+		const GLenum data_type = ToGLDataType(source_format);
+
+		glTexImage2D(GL_TEXTURE_2D, 0, internal_format, m_specs.Width, m_specs.Height, 0, data_format, data_type, static_cast<const void*>(data.data()));
+
+		if (m_specs.GenMipmaps)
+		{
+			glGenerateMipmap(GL_TEXTURE_2D);
+		}
 	}
 
 	void GLTexture2D::Bind(uint32_t unit) const
@@ -179,11 +200,28 @@ namespace ignis
 			return;
 		}
 
-		uint32_t face_size = m_specs.Width * m_specs.Height * BytesPerPixel(source_format);
+		glGenTextures(1, &m_id);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, m_id);
 
-		if (data.size() != face_size * 6)
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, ToGLMinFilter(m_specs.MinFilter));
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, ToGLMagFilter(m_specs.MagFilter));
+
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+		SetData(source_format, data);
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+
+		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+	}
+
+	GLTextureCube::GLTextureCube(const TextureSpecs& specs)
+		: m_specs(specs)
+	{
+		if (m_specs.Width == 0 || m_specs.Height == 0)
 		{
-			Log::CoreError("TextureCube data size mismatch! Expected {} bytes (6 faces), got {}.", face_size * 6, data.size());
+			Log::CoreError("CubeMap dimensions must be non-zero.");
 			return;
 		}
 
@@ -196,9 +234,40 @@ namespace ignis
 		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, ToGLMinFilter(m_specs.MinFilter));
 		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, ToGLMagFilter(m_specs.MagFilter));
 
-		GLenum internal_format = ToGLInternalFormat(m_specs.Format);
-		GLenum data_format = ToGLImageFormat(source_format);
-		GLenum data_type = ToGLDataType(source_format);
+		const GLenum internal_format = ToGLInternalFormat(m_specs.Format);
+
+		// Allocate storage for all 6 faces without initializing data
+		for (int i = 0; i < 6; ++i)
+		{
+			glTexImage2D(
+				GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+				0,
+				internal_format,
+				m_specs.Width,
+				m_specs.Height,
+				0,
+				GL_RGBA,
+				GL_UNSIGNED_BYTE,
+				nullptr
+			);
+		}
+
+		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+	}
+
+	void GLTextureCube::SetData(ImageFormat source_format, std::span<const std::byte> data) const
+	{
+		uint32_t face_size = m_specs.Width * m_specs.Height * BytesPerPixel(source_format);
+
+		if (data.size() != face_size * 6)
+		{
+			Log::CoreError("TextureCube data size mismatch! Expected {} bytes (6 faces), got {}.", face_size * 6, data.size());
+			return;
+		}
+
+		const GLenum internal_format = ToGLInternalFormat(m_specs.Format);
+		const GLenum data_format = ToGLImageFormat(source_format);
+		const GLenum data_type = ToGLDataType(source_format);
 
 		const std::byte* raw_data = data.data();
 
@@ -223,8 +292,6 @@ namespace ignis
 		{
 			glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
 		}
-
-		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
 	}
 
 	void GLTextureCube::Bind(uint32_t unit) const
--- Ignis/src/Ignis/Renderer/Texture.h
+++ Ignis/src/Ignis/Renderer/Texture.h
@@ -73,8 +73,12 @@ namespace ignis
 	{
 	public:
 		virtual ~Texture() = default;
+
 		virtual uint32_t GetWidth() const = 0;
 		virtual uint32_t GetHeight() const = 0;
+
+		virtual void SetData(ImageFormat source_format, std::span<const std::byte> data) const = 0;
+
 		virtual void Bind(uint32_t unit) const = 0;
 		virtual void UnBind() const = 0;
 	};
@@ -83,11 +87,13 @@ namespace ignis
 	{
 	public:
 		static std::shared_ptr<Texture2D> Create(const TextureSpecs& specs, ImageFormat source_format, std::span<const std::byte> data);
+		static std::shared_ptr<Texture2D> Create(const TextureSpecs& specs);
 	};
 
 	class TextureCube : public Texture
 	{
 	public:
 		static std::shared_ptr<TextureCube> Create(const TextureSpecs& specs, ImageFormat source_format, std::span<const std::byte> data);
+		static std::shared_ptr<TextureCube> Create(const TextureSpecs& specs);
 	};
 }
