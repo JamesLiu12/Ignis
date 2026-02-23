b1046c77c602bc761b09f5028e2e5dde60da329c
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2026-02-23 16:08:03 +0800

    feat: add TextureUsage

--- Ignis/src/Ignis/Renderer/Texture.h
+++ Ignis/src/Ignis/Renderer/Texture.h
@@ -38,6 +38,22 @@ namespace ignis
 		Depth24Stencil8
 	};
 
+	enum class TextureUsage : uint32_t
+	{
+		None         = 0,
+		Sampled		 = 1 << 0,
+		RenderTarget = 1 << 1,
+		Depth		 = 1 << 2,
+		Stencil		 = 1 << 3,
+	};
+
+	inline TextureUsage operator|(TextureUsage a, TextureUsage b) {
+		return (TextureUsage)((uint32_t)a | (uint32_t)b);
+	}
+	inline bool HasUsage(TextureUsage u, TextureUsage bit) {
+		return ((uint32_t)u & (uint32_t)bit) != 0;
+	}
+
 	struct TextureSpecs
 	{
 		uint32_t Width = 0;
@@ -48,6 +64,9 @@ namespace ignis
 		TextureFilter MinFilter = TextureFilter::LinearMipmapLinear;
 		TextureFilter MagFilter = TextureFilter::Linear;
 		bool GenMipmaps = true;
+
+		uint32_t Samples = 1;
+		TextureUsage Usage = TextureUsage::Sampled;
 	};
 
 	class Texture : public Asset
