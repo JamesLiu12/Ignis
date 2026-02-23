956ddfac6cfbb2714f9fb95b1e5eed414397d318
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2026-02-23 20:03:34 +0800

    feat: add framebuffer

---
+++ Ignis/src/Ignis/Renderer/Framebuffer.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "Texture.h"
+
+namespace ignis
+{
+	enum class FramebufferAttachmentType { Color, Depth, Stencil, DepthStencil };
+
+	struct FramebufferAttachmentSpecs
+	{
+
+		FramebufferAttachmentSpecs(TextureFormat format)
+			: Format(format) {
+		}
+
+		TextureFormat Format;
+		FramebufferAttachmentType Type;
+	};
+
+	struct FrameBufferSpecs
+	{
+		uint32_t Width = 0;
+		uint32_t Height = 0;
+		uint32_t Samples = 1;
+		std::vector<FramebufferAttachmentSpecs> Attachments;
+	};
+
+	class Framebuffer
+	{
+	public:
+		virtual std::shared_ptr<Texture2D> GetColorAttachment(uint32_t index = 0) const = 0;
+		virtual std::shared_ptr<Texture2D> GetDepthAttachment() const = 0;
+
+		virtual uint32_t GetWidth() const = 0;
+		virtual uint32_t GetHeight() const = 0;
+
+		virtual void Resize(uint32_t width, uint32_t height) = 0;
+		virtual void Invalidate() = 0;
+
+		virtual void Bind() const = 0;
+		virtual void UnBind() const = 0;
+
+		static std::shared_ptr<Framebuffer> Create(const FrameBufferSpecs& specs);
+	};
+}
