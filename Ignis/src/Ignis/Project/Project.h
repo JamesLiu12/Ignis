d3a2d8598422bf1dd1279c717454dc6932ec4da5
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2026-02-25 14:21:23 +0800

    fix: add AssetRegistry to Project

--- Ignis/src/Ignis/Project/Project.h
+++ Ignis/src/Ignis/Project/Project.h
@@ -12,6 +12,7 @@ namespace ignis
 		{
 			std::string ProjectName;
 			std::string AssetDirectory;
+			std::string AssetRegistry;
 			std::string StartScene;
 		};
 
@@ -20,11 +21,13 @@ namespace ignis
 		const std::string& GetProjectName() const { return m_config.ProjectName; }
 		const std::filesystem::path& GetProjectDirectory() const { return m_project_directory; }
 		std::filesystem::path GetAssetDirectory() const { return m_project_directory / m_config.AssetDirectory; }
+		std::filesystem::path GetAssetRegistry() const { return m_project_directory / m_config.AssetRegistry; }
 		std::filesystem::path GetStartScene() const { return GetAssetDirectory() / m_config.StartScene; }
 
 		static const std::string& GetActiveProjectName() { return s_active_project->m_config.ProjectName; }
 		static const std::filesystem::path& GetActiveProjectDirectory() { return s_active_project->m_project_directory; }
 		static std::filesystem::path GetActiveAssetDirectory() { return s_active_project->m_project_directory / s_active_project->m_config.AssetDirectory; }
+		static std::filesystem::path GetActiveAssetRegistry() { return s_active_project->m_project_directory / s_active_project->m_config.AssetRegistry; }
 		static std::filesystem::path GetActiveStartScene() { return s_active_project->GetAssetDirectory() / s_active_project->m_config.StartScene; }
 
 		static std::shared_ptr<Project> GetActive() { return s_active_project; }
