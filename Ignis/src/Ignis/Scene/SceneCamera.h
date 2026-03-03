7cc37a20224f68eb95909df3b40ab9c3dc30d514
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2026-03-03 20:44:41 +0800

    feat: reimplement SceneCamera

--- Ignis/src/Ignis/Scene/SceneCamera.h
+++ Ignis/src/Ignis/Scene/SceneCamera.h
@@ -2,55 +2,55 @@
 
 #include "Ignis/Renderer/Camera.h"
 
-#include <cstdint>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
 
 namespace ignis
 {
 	class SceneCamera : public Camera
 	{
 	public:
-		enum class ProjectionType : uint8_t
-		{
-			Perspective = 0,
-			Orthographic = 1
-		};
+		enum class ProjectionType { Perspective = 0, Orthographic = 1 };
 
-	public:
 		SceneCamera();
 		~SceneCamera() override = default;
 
 		void SetProjectionType(ProjectionType type);
-		ProjectionType GetProjectionType() const;
+		ProjectionType GetProjectionType() const { return m_projection_type; }
 
-		// Perspective
-		void SetPerspectiveVerticalFOV(float fov_deg);
+		void SetAspectRatio(float aspect_ratio);
+		float GetAspectRatio() const { return m_aspect_ratio; }
+
+		void SetPerspective(float fov_deg, float near_clip, float far_clip);
+
+		float GetPerspectiveFOV()       const { return m_perspective_fov; }
+		float GetPerspectiveNearClip()  const { return m_perspective_near; }
+		float GetPerspectiveFarClip()   const { return m_perspective_far; }
+
+		void SetPerspectiveFOV(float fov_deg);
 		void SetPerspectiveNearClip(float near_clip);
 		void SetPerspectiveFarClip(float far_clip);
 
-		float GetPerspectiveVerticalFOV() const;
-		float GetPerspectiveNearClip() const;
-		float GetPerspectiveFarClip() const;
+		void SetOrthographic(float size, float near_clip, float far_clip);
 
-		// Orthographic
+		float GetOrthographicSize()     const { return m_orthographic_size; }
+		float GetOrthographicNearClip() const { return m_orthographic_near; }
+		float GetOrthographicFarClip()  const { return m_orthographic_far; }
+
 		void SetOrthographicSize(float size);
 		void SetOrthographicNearClip(float near_clip);
 		void SetOrthographicFarClip(float far_clip);
 
-		float GetOrthographicSize() const;
-		float GetOrthographicNearClip() const;
-		float GetOrthographicFarClip() const;
+		void SetViewFromWorldTransform(const glm::mat4& world_transform);
 
-		// Viewport
-		void SetViewportSize(uint32_t width, uint32_t height);
-		void SetAspectRatio(float aspect_ratio);
-		float GetAspectRatio() const;
-
+	private:
 		void RecalculateProjection();
 
 	private:
 		ProjectionType m_projection_type = ProjectionType::Perspective;
 
-		float m_perspective_fov_deg = 45.0f;
+		float m_perspective_fov = 45.0f;
 		float m_perspective_near = 0.01f;
 		float m_perspective_far = 1000.0f;
 
