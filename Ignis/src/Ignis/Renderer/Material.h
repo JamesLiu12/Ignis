b78ed5aaded4884adcad74275ccf0acfd32f4d39
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2026-03-04 22:37:53 +0800

    refactor: separate MaterialData and Material

--- Ignis/src/Ignis/Renderer/Material.h
+++ Ignis/src/Ignis/Renderer/Material.h
@@ -8,26 +8,6 @@
 
 namespace ignis
 {
-	enum class MaterialType
-	{
-		Albedo,
-		Normal,
-		Metal,
-		Roughness,
-		Emissive,
-		AO
-	};
-
-	struct MaterialData
-	{
-		AssetHandle AlbedoMap = AssetHandle::Invalid;
-		AssetHandle NormalMap = AssetHandle::Invalid;
-		AssetHandle MetalnessMap = AssetHandle::Invalid;
-		AssetHandle RoughnessMap = AssetHandle::Invalid;
-		AssetHandle EmissiveMap = AssetHandle::Invalid;
-		AssetHandle AOMap = AssetHandle::Invalid;
-	};
-
 	class Material
 	{
 	public:
--- Ignis/src/Ignis/Scene/Components.h
+++ Ignis/src/Ignis/Scene/Components.h
@@ -2,7 +2,7 @@
 
 #include "SceneCamera.h"
 #include "Ignis/Core/UUID.h"
-#include "Ignis/Renderer/Material.h"
+#include "Ignis/Renderer/MaterialData.h"
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
