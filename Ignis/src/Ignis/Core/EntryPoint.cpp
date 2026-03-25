6ccb785c9fcf00fbfada3f1dc7b76f6f939f9ed9
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2026-03-25 22:19:31 +0800

    fix: let Logger shutdown in main

--- Ignis/src/Ignis/Core/EntryPoint.cpp
+++ Ignis/src/Ignis/Core/EntryPoint.cpp
@@ -1,9 +1,13 @@
 #include "EntryPoint.h"
+#include "Ignis/Core/Log.h"
 
 int main(int argc, char** argv)
 {
 	puts("Hi, Ignis");
-	auto app = ignis::Application::Create();
-	app->Run();
+	{
+		auto app = ignis::Application::Create();
+		app->Run();
+	}
+	ignis::Log::Shutdown();
 	return 0;
 }
